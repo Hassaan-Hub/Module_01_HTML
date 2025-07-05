7559ddff595176587e31693e7996aea3f4d67d11
hassaan-hub <hassaanali994@gmail.com>
2025-07-06 00:27:31 +0500

    class_3_java

---
+++ javascript/class_03/app.js
@@ -0,0 +1,34 @@
+var nam = 1;
+console.log(nam++);
+
+nam++;
+console.log(nam);
+
+var nam = 1;
+++nam;
+console.log(nam++);
+console.log(++nam);
+
+var nam = 5;
+nam--;
+console.log(nam--);
+console.log(nam);
+
+var num = 12;
+--num;
+console.log(num);
+
+var totacost = 1 + 2 + 5;
+console.log(totacost);
+
+var num = 4 * 90;
+console.log(num);
+
+var num = -4 + 9;
+console.log(num);
+
+var num = 63 + 7;
+console.log(num);
+
+
+
