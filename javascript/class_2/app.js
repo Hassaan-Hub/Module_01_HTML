b5ce296e1a9a13ce2fe0b26f6ba82bb883429748
hassaan-hub <hassaanali994@gmail.com>
2025-07-05 14:53:55 +0500

    class_2_java

---
+++ javascript/class_2/app.js
@@ -0,0 +1,43 @@
+var nam = "Hello World"
+alert(nam)
+
+alert("Hassan Ali")
+console.log("Hello World");
+
+
+var nam1 = 25;
+var nam2 = 30;
+var nam3 = nam1 + nam2;
+console.log(nam3);
+
+var nam = 1000;
+var nam1 = 909;
+var nam2 = nam - nam1;
+console.log(nam2);
+
+var nama = 8897;
+var namb = 786;
+var namc = nama / namb;
+console.log(namc);
+
+var nam1 = 100000;
+var nam2 = 400000;
+var nam3 = nam1 * nam2;
+console.log(nam3);
+
+var nam = 175000;
+var nam1 = 68000;
+var nam2 = nam - nam1;
+console.log(nam2);
+
+var nam = 10;
+var nam1 = 50;
+console.log(nam % nam1);
+
+var nam = 500;
+var nam0 = 367;
+console.log(nam % nam0);
+
+var nam = 100;
+var nam0 = 73;
+console.log(nam % nam0);
