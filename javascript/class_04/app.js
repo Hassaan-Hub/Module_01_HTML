7764d53d81c3fec8221be10ce43026635e800f7c
hassaan-hub <hassaanali994@gmail.com>
2025-07-07 07:40:04 +0500

    class_4_java

---
+++ javascript/class_04/app.js
@@ -0,0 +1,17 @@
+var firstname = "Ali";
+var secondname = "Hassan";
+console.log(firstname + secondname);
+
+
+
+
+var name1 = "karachi"
+var name2 = "Rochniosheher"
+var custom = name1 + name2;
+
+console.log(custom);
+alert(custom)
+
+
+var alpha = 6 * 7;
+alert(alpha) 
