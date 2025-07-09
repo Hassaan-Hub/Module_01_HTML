6c5d82ca663b49b24ecc02f05ed9267cb97c138e
hassaan-hub <hassaanali994@gmail.com>
2025-07-09 13:58:50 +0500

    class_5_updated

---
+++ javascript/class_05/app.js
@@ -0,0 +1,50 @@
+var fullname = "Mark"
+if(fullname === "Mark"){
+    console.log("Mark is in the class!");
+    
+}
+
+var name = "hello"
+if(name === "hello"){
+    console.log("hello guys my new vloge!");
+    
+}
+
+var num1 = 23;
+if(num1 >= 23){
+    console.log("true!");
+    
+}
+var num = 23;
+if(num <= 23){
+    console.log("true!");
+    
+}
+
+
+var condition = "next"
+if(condition == "next"){
+    console.log("Hackathon Postpone!");
+    
+}
+
+var condition = "hackathon"
+if(condition == "hackathon"){
+    console.log("Hackathon Postpone!");
+}else{
+    console.log("Hackathon is on time!");
+    
+}
+
+if(condition == "sunny"){
+    console.log("Hackathon is on time!");    
+}
+
+var country = prompt("Where do you live?")
+if(country == "Pakistan"){
+    alert("Pakistan zindabad")
+}else if(country == "Dubai"){
+    alert("apke to maze")
+}else if(country == "America"){
+    alert("Nice country")
+}else alert("Jeye Saad Hussain Rizwi")
