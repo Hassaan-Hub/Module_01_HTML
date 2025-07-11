7ed9efccea20af447b466d8662e1f702c1fc5e0a
hassaan-hub <hassaanali994@gmail.com>
2025-07-11 10:56:58 +0500

    class_9_remaining

--- javascript/class_09/app.js
+++ javascript/class_09/app.js
@@ -1,30 +1,69 @@
-for(var i = 0; i < 10; i++){
-    console.log(i + 1,"Please follow me on github !");
-}
+// for(var i = 0; i < 10; i++){
+//     console.log(i + 1,"Please follow me on github !");
+// }
 
-var fruits = ["Apple", "Mango", "Banana", "Grapes", "BB", "Kiwi", "Papaya"]
+// var fruits = ["Apple", "Mango", "Banana", "Grapes", "BB", "Kiwi", "Papaya"]
 
-for (var i = 0; i < fruits.length; i++){
-    if("Banana" == fruits [i]){
-        console.log("Banana Found at index" , i);
-        break;
-    }else{
-        console.log(i);
-    }
-}
+// for (var i = 0; i < fruits.length; i++){
+//     if("Banana" == fruits [i]){
+//         console.log("Banana Found at index" , i);
+//         break;
+//     }else{
+//         console.log(i);
+//     }
+// }
 
-console.log(fruits[0]);
+// console.log(fruits[0]);
 
 
 
-for (var i = 0; i < fruits.length; i++){
-    console.log(fruits[i]);   
-}
+// for (var i = 0; i < fruits.length; i++){
+//     console.log(fruits[i]);   
+// }
 
-console.log(" ");
-var table = +prompt("Enter your Number")
+// console.log(" ");
+// var table = +prompt("Enter your Number")
 
-for(var i = 1; i <= 10; i++){
-    console.log(table + "X" + i + "=" +  table * i);
-    
+// for(var i = 1; i <= 10; i++){
+//     console.log(table + " X " +  i  + " = " +  table * i);
+// }
+
+
+
+// var firstNames = ["BlueRay ", "Upchuck ", "Lojack ", "Gizmo ", "Do-Rag "];
+
+// var lastName = ["Zzz", "Burp", "Dogbone", "Droop"];
+
+// var fullNames = [];
+
+// for (var i = 0; i < firstNames.length; i++) {
+
+//     for (var j = 0; j < lastName.length; j++) {
+//         fullNames.push(firstNames[i] + lastName[j])
+//     }
+
+// }
+// console.log(fullNames);
+
+// for(var i = 1; i<= 10; i++){
+//     row = "";
+//     for(var j = 1; j <= i; j++){
+//         row += "*";
+//     }
+//     console.log(row);
+// }
+
+// for (let i = 1; i <= 100; i++) {
+//     console.log(`Multiplication table for ${i}:`);
+
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} X ${j} = ${i * j}`);
+//     }
+
+//     console.log('');
+// }
+
+
+for (var i = 1; i <=5; i++){
+    console.log(i);
 }
