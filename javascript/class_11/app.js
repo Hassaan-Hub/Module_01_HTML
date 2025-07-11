6da72c2eaaa1442e84037faf7e582a02ec42ef43
hassaan-hub <hassaanali994@gmail.com>
2025-07-11 20:57:20 +0500

    class_11_remaining

---
+++ javascript/class_11/app.js
@@ -0,0 +1,10 @@
+var text = "Hello guys my new vloge"
+
+console.log(text.length);
+var findName = text.lastIndexOf("s")
+console.log(findName);
+
+console.log(text.slice(0 , 5));
+console.log(text.slice(findName + findName + 3));
+
+console.log(text.slice(11,15));
