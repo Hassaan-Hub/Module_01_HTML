c3de115f733cf036d07e405ae2f785429cf3fbe2
hassaan-hub <hassaanali994@gmail.com>
2025-07-14 10:55:50 +0500

    class_11_completed

--- javascript/class_11/app.js
+++ javascript/class_11/app.js
@@ -9,23 +9,43 @@
 
 // console.log(text.slice(11,15));
 
-var text = "In 1939 World War II happens"
-console.log(text.slice(0, 8));
+// var text = "In 1939 World War II happens"
+// console.log(text.slice(0, 8));
 
-for (var i = 0; i < text.length; i++) {
-    if (text.slice(i, i + 12) === "World War II") {
-        text = text.slice(0, i) + "The Second World War" + text.slice(i + 12);
-    }
-    console.log(text);
-}
+// for (var i = 0; i < text.length; i++) {
+//     if (text.slice(i, i + 12) === "World War II") {
+//         text = text.slice(0, i) + "The Second World War" + text.slice(i + 12);
+//     }
+//     console.log(text);
+// }
 
-var modifytext = ("Hassaan Farid Kaif Bhai !")
-console.log(modifytext.charAt(modifytext.length - 25));
-
-
-
-var name = "Hassaan"
-console.log(name.slice(0, 2));
-console.log(name.charAt(name.length - 7));
+// var modifytext = ("Hassaan Farid Kaif Bhai !")
+// console.log(modifytext.charAt(modifytext.length - 25));
 
 
+
+// var name = "Hassaan"
+// console.log(name.slice(0, 2));
+// console.log(name.charAt(name.length - 7));
+
+
+// var text = "Hello ! my name is Hassaan !!"
+
+// for (var i = 0; i < text.length; i++){
+//     if (text.charAt(i) === ("!")){
+//         console.log("Exclamation point found!");
+//     break;       
+//     }
+//     console.log(i);
+// }
+
+// var text = "This is cat. Parrot is very cute !"
+// console.log(text.replace(/cat/g,"Parrot"));
+
+// var num = "1.4"
+// console.log(Math.round(num));
+// console.log(Math.floor(num));
+// console.log(Math.ceil(num));
+
+console.log(Math.random());
+console.log(Math.floor(Math.random() * 10 + 1));
