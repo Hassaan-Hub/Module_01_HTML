04d1fb361cbf040c90dcf5c0cb524f8332476745
hassaan-hub <hassaanali994@gmail.com>
2025-07-15 13:49:08 +0500

    class_12_completed

---
+++ javascript/class_12/app.js
@@ -0,0 +1,27 @@
+// var userGuess = +prompt("Guess the Number!");
+// var randamNumber = Math.floor(Math.random() * 10 + 1);
+
+// console.log(randamNumber);
+
+// if (userGuess === randamNumber){
+//     alert("Your Guess is Correct!")
+// }else{
+//     alert("Your Guess is Wrong!")
+// }
+
+// var num1 = ("10");
+// var num1 = parseInt(num1)
+// var num1 = parseFloat(num1)
+
+// var num2 = 5;
+// sum = num1 + num2
+
+
+// console.log(sum);
+
+var num = 123456789
+console.log(num.toString());
+
+var randomNumber = Math.random() * 100 + 1
+var randomNumber = 1.555;
+console.log(+randomNumber.toFixed());
