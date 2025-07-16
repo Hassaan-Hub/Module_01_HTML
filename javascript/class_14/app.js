fcadd58b66f7c6fee87acaccf5f2e59324ccdba0
hassaan-hub <hassaanali994@gmail.com>
2025-07-16 22:43:03 +0500

    class_14_completed

---
+++ javascript/class_14/app.js
@@ -0,0 +1,68 @@
+// function printName(){
+//     console.log("Hassaan445");
+// }
+
+// printName();
+// for (var i = 0; i < 5; i++){
+//     console.log(i);
+// }
+// printName();
+
+
+// function sum (){
+//     console.log(2 + 2);
+// }
+
+// function tellTime(){
+//     var now = new Date()
+//     var theHour = now.getHours()
+//     var theMin = now.getMinutes()
+//     var theSec = now.getSeconds()
+//     var theMlse = now.getMilliseconds()
+//     console.log("Current time: " + theHour + ":" + theMin + ":" + theSec + ":" + theMlse);
+// }
+// tellTime()
+
+// function printName(){
+//     console.log("Ahmed");
+// }
+// printName()
+
+// function printMyName(firstname, lastname){
+//     console.log(firstname + lastname);
+    
+// }
+
+// printMyName("hello guys " , "my new vlog")
+// printMyName("Muhammad " , "Ahmed")
+// printMyName("Hass" , "an")
+
+function add(num1 , num2){
+    console.log("Addition:" , num1 + num2);
+}
+
+function subtract(num1 , num2){
+    console.log("Subtraction:" , num1 - num2);
+}
+
+function sum(num1 , num2){
+    return num1 + num2
+}
+
+add(2 , 2);
+subtract(12 , 5);
+
+const result = sum(2, 2);
+console.log("Sum:", result); 
+
+function subtract(num1){
+    return num1 - sum(1, 5)
+}
+
+console.log(subtract(9));
+
+
+var sum = (2, 7);
+var res = (5, 9);
+console.log(sum);
+console.log(res);
