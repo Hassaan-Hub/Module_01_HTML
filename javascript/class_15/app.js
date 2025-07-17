8dec4756cc1bced94ddce3ef3e4685b4684fe27a
hassaan-hub <hassaanali994@gmail.com>
2025-07-17 12:00:04 +0500

    class_15_completed

--- javascript/class_15/app.js
+++ javascript/class_15/app.js
@@ -1,15 +1,56 @@
-function printMyFullName(fname, lname){
-    console.log(`${fname}  ${lname}`);
+// function printMyFullName(fname, lname){
+//     console.log(`${fname}  ${lname}`);
+// }
+
+// printMyFullName("Ali" , "Haider")
+// printMyFullName("its" , "me")
+// printMyFullName("hassaan" , "hii");
+
+// function printTable(tableNumber){
+//     for (var i = 1; i <=10; i++){
+//         console.log(`${tableNumber} X ${i} = ${tableNumber *i}`);
+//     }
+// }
+
+// printTable(9);
+
+
+// function add(firstname , second){
+//     var sum = firstname + second
+//     return sum
+// }
+
+// var res = add(4, 9)
+// var res1 = add(8 , 2)
+
+// console.log(res);
+// console.log(res1);
+
+// var weekDay = prompt("Enter a Week Day !").toLowerCase();
+
+// if("fri" === weekDay || "sat" === weekDay || "sun" === weekDay);
+
+// if("fri" === weekDay){   
+// }else if("sat" === weekDay){
+// }else if("sun" === weekDay){}
+
+// switch (weekDay){
+//     case "fri":
+//         alert("Biryani Day!")
+//         break;
+//     case "sat":
+//         alert("Weekend Saturday off!")
+//         break;
+//     case "sun":
+//         alert("Be ready for Monday!")
+//         break;
+//     default : alert("Shoot")
+// }
+
+var i = 1;
+
+while(true){
+    console.log(`${2} X ${i} = ${2 * i} `);
+    i++;
+    break;
 }
-
-printMyFullName("Ali" , "Haider")
-printMyFullName("its" , "me")
-printMyFullName("hassaan" , "hii");
-
-function printTable(tableNumber){
-    for (var i = 1; i <=10; i++){
-        console.log(`${tableNumber} X ${i} = ${tableNumber *i}`);
-    }
-}
-
-printTable(9);
