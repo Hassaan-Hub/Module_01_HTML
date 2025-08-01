66f6494662b42c8ca2082db7da199773bab561d6
hassaan-hub <hassaanali994@gmail.com>
2025-08-01 11:34:36 +0500

    class_22_completed

---
+++ javascript/class_22/app.js
@@ -0,0 +1,27 @@
+function generateBgColor(){
+    var hexColor = Math.random().toString(16).slice(2 , 8).padEnd(6 , 0)
+    var body = document.querySelector('body')
+    var hexField = document.querySelector('#hex-code')
+    var generatedCode = `#${hexColor}`
+
+    hexField.innerHTML = generatedCode
+
+    body.style.backgroundColor = generatedCode
+    body.style.transition = '0.4s ease-in all'
+}
+
+setInterval(generateBgColor, 1000)
+
+var initialRound = 0
+
+function roundBox(){
+    var box = document.querySelector('.box')
+    var hexColor = Math.random().toString(16).slice(2, 8).padEnd(6, 0)
+
+    var round = initialRound += 90;
+    box.style.rotate = `${round}deg`
+    box.style.backgroundColor = `#${hexColor}`
+    box.style.transition = '0.5s ease-in-out all'
+}
+
+setInterval(roundBox, 100)
