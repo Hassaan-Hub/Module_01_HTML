99e2893add25398f275cb6aa5df1499825ed5070
hassaan-hub <hassaanali994@gmail.com>
2025-08-02 18:54:01 +0500

    class_23_completed

--- javascript/class_23/app.js
+++ javascript/class_23/app.js
@@ -1,7 +1,47 @@
-var person = {
-    name1: "Ahmed",
-    name2: "Ahmed",
-    name3: "Ahmed"
+// var person = {
+//     name: "Hassaan",
+//     age: "17",
+//     roll: "123",
+//     isPermenant: true,
+//     greet: function () {
+//         return "welcome " + this.roll;
+//     },
+//     shift: ["morning", "afternoon", "evening"],
+//     shift: {
+//         morning: "9:00 AM",
+//         afternoon: "1:00 PM",
+//         evening: "5:00 PM"
+//     }
+// }
+
+//console.log(Object.keys(person));
+// console.log(Object.values(person));
+// console.log(Object.entries(person));
+
+
+
+// for (key in person) {
+//     if (person[key] === "Ali") {
+//         console.log("Ali is FOUND !");
+//     } else {
+//         console.log("Ali is NOT FOUND !");
+//     }
+//     break;
+// }
+
+// person.age = 6
+
+// console.log(person);
+
+
+
+function Person(name, age, roll, isPermenant){
+    this.name1 = name;
+    this.age1 = age;
+    this.roll1 = roll;
+    this.permenant1 = isPermenant;
 }
 
-console.log(Object.keys(person));
+var person1 
+var person2
+var person3
