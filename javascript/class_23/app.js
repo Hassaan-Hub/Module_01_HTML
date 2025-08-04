8b17132313c44b0dbf1c9cdd6a6319394ffbd803
hassaan-hub <hassaanali994@gmail.com>
2025-08-04 13:56:27 +0500

    class_23_completed

--- javascript/class_23/app.js
+++ javascript/class_23/app.js
@@ -35,13 +35,79 @@
 
 
 
-function Person(name, age, roll, isPermenant){
-    this.name1 = name;
-    this.age1 = age;
-    this.roll1 = roll;
-    this.permenant1 = isPermenant;
-}
+// function Person(name, age, roll, isPermenant){
+//     this.name1 = name;
+//     this.age1 = age;
+//     this.role1 = role;
+//     this.permenant1 = isPermenant;
+// }
 
-var person1 
-var person2
-var person3
+// var person1 = new Person ("Hassaan", 18, "Teacher", true)
+// var person2 = new Person ("Majid", 15, "student", false)
+// var person3 = new Person ("Qasum", 17, "student", true)
+// var person4 = new Person ("kaif", 12, "student", false)
+
+// console.log(person1);
+// console.log(person2);
+// console.log(person3);
+// console.log(person4);
+
+
+
+// var persons = [
+//     {
+//         name: "Hassaan",
+//         age : 18,
+//         role: "teacher",
+//         isPermenant: true,
+//         greet: function(){
+//             return `hello my is ${this.name}`
+//         }
+//     },
+//     {
+//         name: "Majid",
+//         age : 15,
+//         role: "student",
+//         isPermenant: false,
+//         greet: function(){
+//             return `Hello my name is ${this.name}`
+//         }
+//     },
+//     {
+//         name: "Ali",
+//         age : 11,
+//         role: "student",
+//         isPermenant: true,
+//         greet: function(){
+//             return `Hello my name is ${this.name}`
+//         }
+//     }
+// ]
+// console.log(persons[2].greet())
+
+// console.log(persons[0].name);       // output is Hassaan
+// console.log(persons[1].greet());    // output is Hello my name is Majid
+// persons.forEach(person =>{             
+//     console.log(person.greet());       // output is greet oski puri length cover kare ga 
+// })
+
+
+
+var person = [
+    {
+        name: "Hassaan",
+        age : 23,
+        role: "teacher",
+        isPermenant: true,
+        greet: function(audience){
+            return `Hello ${audience}, My name is ${this.name} and I am a ${this.role}.`
+        }
+    },
+]
+
+delete person.age;
+
+// console.log(person[0].greet("Teacher"));
+// console.log(person[0].greet("Student"));
+
+console.log(person);
