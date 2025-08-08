2d887080ec35cd6bae0d3d0ef8c771c98d2e1b96
hassaan-hub <hassaanali994@gmail.com>
2025-08-08 10:45:16 +0500

    class_25_completed

--- javascript/class_25/app.js
+++ javascript/class_25/app.js
@@ -1,108 +1,108 @@
-// var idCount = 1;
+var idCount = 1;
 
-// function addTodo(){
-//     var input = document.getElementById('inp')
-//     var todo = document.getElementById('todos')
+function addTodo(){
+    var input = document.getElementById('inp')
+    var todo = document.getElementById('todos')
 
-//     var element = document.createElement('div')
-//     var pera = document.createElement('p')
-//     var deleteBtn = document.createElement('button')
-//     var updateBtn = document.createElement('button')
+    var element = document.createElement('div')
+    var pera = document.createElement('p')
+    var deleteBtn = document.createElement('button')
+    var updateBtn = document.createElement('button')
 
-//     deleteBtn.textContent = "Delete";
-//     updateBtn.textContent = "Update";
+    deleteBtn.textContent = "Delete";
+    updateBtn.textContent = "Update";
 
-//     deleteBtn.setAttribute("onclick", `deleteTodo(${idCount})`)
-//     updateBtn.setAttribute("onclick", `updateTodo(${idCount})`)
+    deleteBtn.setAttribute("onclick", `deleteTodo(${idCount})`)
+    updateBtn.setAttribute("onclick", `updateTodo(${idCount})`)
 
-//     element.setAttribute('id', idCount)
-//     pera.textContent = input.value;
+    element.setAttribute('id', idCount)
+    pera.textContent = input.value;
 
-//     element.appendChild(pera)
-//     element.appendChild(deleteBtn)
-//     element.appendChild(updateBtn)
+    element.appendChild(pera)
+    element.appendChild(deleteBtn)
+    element.appendChild(updateBtn)
     
-//     todo.appendChild(element);
+    todo.appendChild(element);
 
-//     idCount = idCount + 1;
-//     input.value = ""
-// }
+    idCount = idCount + 1;
+    input.value = ""
+}
 
-// function updateTodo(id){
-//     var element = document.getElementById(id)
-//     var peraElem = element.querySelector('p')
-//     var input = document.getElementById('inp');
+function updateTodo(id){
+    var element = document.getElementById(id)
+    var peraElem = element.querySelector('p')
+    var input = document.getElementById('inp');
     
-//     peraElem.textContent = input.value
-//     input.value = ""
-// }
+    peraElem.textContent = input.value
+    input.value = ""
+}
 
-// function deleteTodo(id){
-//     var element = document.getElementById(id)
+function deleteTodo(id){
+    var element = document.getElementById(id)
 
-//     var parentElem = element.parentNode;
-//     parentElem.removeChild(element)
-//     input.value = ""
-// }
-
-
-// var object = {
-//     first : 1,
-//     second: 2,
-//     third : 3
-// }
-
-// // console.log(Object.keys(object));
-// for (key in object){
-//     if (object[key] === 1){
-//         console.log("first is found !");
-//     }else{
-//         console.log("first is not found !");
-//     }break;
-// }
+    var parentElem = element.parentNode;
+    parentElem.removeChild(element)
+    input.value = ""
+}
 
 
-// var num = 10;
+var object = {
+    first : 1,
+    second: 2,
+    third : 3
+}
 
-// console.log(num.toFixed(2));
-// console.log(num);
-
-// var name = "alia"
-// console.log(name.lastIndexOf('a'));
-
-// var name1 = "john player"
-// console.log(name1[6]);
-
-// var city = ["karachi", "lahore", "punjab"]
-// console.log(city);
-// city.length = 0;
-// city.push("balochistan")
-// console.log(city);
+// console.log(Object.keys(object));
+for (key in object){
+    if (object[key] === 1){
+        console.log("first is found !");
+    }else{
+        console.log("first is not found !");
+    }break;
+}
 
 
-// var a = 7;
-// function Hello(){
-//     if (true){
-//         a = 5
-//     }
-//     console.log(a);
-// }
-// Hello()
+var num = 10;
 
-// var size = 5;
-// var a = 5;
-// for(var i = size; i >=0; i--){
-//     a = a-2
-//     console.log(a);
-// }
+console.log(num.toFixed(2));
+console.log(num);
+
+var name = "alia"
+console.log(name.lastIndexOf('a'));
+
+var name1 = "john player"
+console.log(name1[6]);
+
+var city = ["karachi", "lahore", "punjab"]
+console.log(city);
+city.length = 0;
+city.push("balochistan")
+console.log(city);
 
 
-// // a = 5
-// function rang(length){
-//     for(var i = 0; i <=length; i++){
-//         // i++
-//         console.log(i);
-//     }
+var a = 7;
+function Hello(){
+    if (true){
+        a = 5
+    }
+    console.log(a);
+}
+Hello()
+
+var size = 5;
+var a = 5;
+for(var i = size; i >=0; i--){
+    a = a-2
+    console.log(a);
+}
+
+
+// a = 5
+function rang(length){
+    for(var i = 0; i <=length; i++){
+        // i++
+        console.log(i);
+    }
     
-// }
-// rang(6)
+}
+rang(6)
