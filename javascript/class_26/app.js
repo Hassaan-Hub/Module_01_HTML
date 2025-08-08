75fb68302d45d8d2575a5d493dcc2e1e93f559bb
hassaan-hub <hassaanali994@gmail.com>
2025-08-08 16:39:18 +0500

    class_26_completed

---
+++ javascript/class_26/app.js
@@ -0,0 +1,40 @@
+// var num = 22;
+// var num = "abc";
+// var num = "dsf";
+
+// function abc(){
+//     var num = 24;
+//     console.log(num);  //  output 24
+// }abc()
+
+// console.log(num);     // output dsf
+
+// console.log(num);    //  output dsf
+
+// {
+//     let num = "hello";
+//     console.log(num);   // output is hello
+// }
+// console.log(num);       //  output is error ager let or const hoga to var hoga to error nahi aaye ga
+
+// function asd(){
+//     let num;            // yaha pe sirif declear kia hai
+//     console.log(num);
+    
+// }
+// num = "output is"       // yaha pe define kia hai
+// console.log(num);   
+
+// let num = "what's app guys"
+// console.log(num);
+
+// asd()
+
+// const num = num;  //ReferenceError: Cannot access 'num' before initialization
+
+// num = 24
+
+// console.log(num);
+
+const num = "plain crash"
+console.log(num);
