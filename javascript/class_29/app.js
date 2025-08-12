c33d08ea806742bee083dacef041265b92564b63
hassaanhub454@gmail.com <hassaanali994@gmail.com>
2025-08-12 10:17:38 +0500

    class_29_udated

---
+++ javascript/class_29/app.js
@@ -0,0 +1,43 @@
+// var x;
+// console.log(x)
+
+// function a(){
+//     x = 123
+//     console.log(x);
+// }
+// a()
+
+// function b(){
+//     x = 78
+//     console.log(x);
+// }
+// b()
+
+
+// function c(){
+//     console.log("ratak");
+    
+// }
+// c()
+
+// function d(){
+//     console.log("chiken");
+// }
+// d()
+
+
+// before
+
+// let sum = (num1 , num2)=>{
+//     return num1 + num2
+// }
+
+// after
+
+// let sum = (num1 , num2) => num1 + num2
+
+// var result = sum(5 , 15)
+
+// console.log(result);
+
+// c()
