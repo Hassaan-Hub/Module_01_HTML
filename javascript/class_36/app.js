b83b1fe1e49647cfb1500c8a6100c3dbbecfe66e
hassaanhub454@gmail.com <hassaanali994@gmail.com>
2025-08-16 14:59:44 +0500

    class_36_completed

---
+++ javascript/class_36/app.js
@@ -0,0 +1,23 @@
+// console.log("start");
+
+// setTimeout(function abc(){
+//     console.log("I am good");
+// },1000)
+
+// console.log("Hello Bro");
+
+// console.log("how are you");
+
+
+
+// setTimeout(()=>{
+//     console.log("Call Back");
+// }, 1000)
+
+// fetch("https://github.com/Hassaan-Hub").then(
+//     function fetchResolve(){
+//         console.log("API RESOLVE");
+//     }
+// )
+
+// console.log("END");
