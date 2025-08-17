4dd50e1e89169f355a2c9bd90bd52d913f3b1c65
hassaanhub454@gmail.com <hassaanali994@gmail.com>
2025-08-17 19:20:33 +0500

    class_38_updated

---
+++ javascript/class_38/app.js
@@ -0,0 +1,70 @@
+// var arr = [2, 4, 6, 8, 10]
+
+const dummyData = [
+    {
+        id: 1,
+        name: "Hassaan",
+        rollno: 123
+    },
+    {
+        id: 2,
+        name: "Majid",
+        rollno: 321
+    },
+    {
+        id: 3,
+        name: "Kaif",
+        rollno: 432
+    },
+]
+
+// var data = document[2]?.rollno
+// var data = document[2]?.class.one
+
+// if(data == undefined){
+//     console.log("Sahi chal rahi hai mazay kro !!");
+// }else{
+//     console.log("Production phat Gayi !!");
+// }
+
+
+
+// ======================================================== yaha se lekar
+
+// var arr = [2, 4, 6, 8, 10]
+
+// const out = arr.map(function (h){
+//     let result = h * 2
+
+//     return result
+// })
+
+// console.log(out);
+
+
+// var arr = [2, 4, 6, 8, 10]
+
+// const out = arr.map((x) =>{
+//     let result = x * 3
+
+//     return result
+// })
+// console.log(out)
+
+// var arr = [2, 4, 6, 8, 10]
+// const out = arr.map(x => x * 4)
+
+// console.log(out)
+
+
+// const out = dummyData.map((x) =>{
+//     return {
+//         id: x.id,
+//         name: x.name,
+//         rollno: x.rollno
+//     }
+// })
+// console.log(out)
+
+
+//============================================================ yaha tak aske sare method hain
