99475cd87f19dd486073cf432a92e92c00ec3858
hassaanhub454@gmail.com <hassaanali994@gmail.com>
2025-08-20 23:33:53 +0500

    class_39_updated

---
+++ javascript/class_39/app.js
@@ -0,0 +1,50 @@
+// Enhanced object literals
+
+// let person = {
+//     name   : "Hassaan",
+//     rollno : 123,
+//     cource : "Web and Mobile App Development"
+// }
+
+// console.log(person.name,person.rollno)
+
+
+// let name = "Hassaan"
+// let rollno = 213
+// let cource = "web and mobile app development"
+
+// let person = {
+//     name, 
+//     rollno, 
+//     cource,
+//     greet(){
+//         console.log("Hello" , this.name);
+//     }
+// }
+
+// console.log(person.name);
+// person.greet()
+
+
+
+// let obt = {
+//     dynamicKey: "hello world"
+// }
+// console.log(obt.dynamicKey);
+
+
+// let x = "rollno"
+
+// let obt1 = {
+//     [x]: 123
+// }
+
+// console.log(Object.values(obt1)[0]);
+
+
+
+
+
+
+
+
