3a77e9cc7049bc78a95dc2485cd747d49b1cc7cf
hassaanhub454@gmail.com <hassaanali994@gmail.com>
2025-08-24 19:06:54 +0500

    updated

--- javascript/class_40/app.js
+++ javascript/class_40/app.js
@@ -21,16 +21,64 @@
 // * sb ko import krne ki zarurat nhi hoti
 // * import krne ke liye export hona chahiye
 
-import * as utils from "./utils.js"
+// import * as utils from "./utils.js"
 
-console.log(utils.multiply(2 , 2) + " Multiplition");
+// console.log(utils.multiply(2 , 2) + " Multiplition");
 
-let mulFunc = utils.multiply(4 , 2)
-let diviFunc = utils.divide(3 , 10)
-let subtFunc = utils.subtract(3 , 14)
-let addFunc = utils.default(3 , 5)
+// let mulFunc = utils.multiply(4 , 2)
+// let diviFunc = utils.divide(20 , 10)
+// let subtFunc = utils.subtract(20 , 14)
+// let addFunc = utils.default(12 , 5)
 
-console.log(mulFunc + " Multiplition");
-console.log(diviFunc + " Divide");
-console.log(subtFunc + " Subtraction");
-console.log(addFunc + " addition");
+// console.log(mulFunc + " Multiplition");
+// console.log(diviFunc + " Divide");
+// console.log(subtFunc + " Subtraction");
+// console.log(addFunc + " addition");
+
+
+
+// =======================================================
+// Third way to import
+
+// import addfunc, {multiply as mulFunc, divide as diviFunc, subtract} from "./utils.js"
+
+// let addition = addfunc(2 , 3)
+// let multiplition = mulFunc(4 , 2)
+// let divide = diviFunc(10 , 2)
+// let subtraction = subtract(10 , 5)
+
+// console.log(addition + " Addition")
+// console.log(multiplition + " Multiplition")
+// console.log(divide + " Divide")
+// console.log(subtraction + " Subtraction")
+
+
+
+
+// for (let value of [1, 2, 3, 4, 5]) {
+//     console.log(value);
+// }
+
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// for(let i = 0; i < arr.length; i+=4){
+//     console.log(arr[i]);
+// }
+
+
+// +=, -=, *=, /=, %=, **=
+
+// let has = 0;
+// for (let i = 0; i < 10; i++) {
+//     has += i
+//     console.log(has);
+// }
+
+let hasa = 2;
+
+for (let i = 1; i < 10; i++){
+    hasa *= i
+    console.log(hasa);
+    
+}
