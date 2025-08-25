b5d708174efcdf95a0a2f5ec42d3234119c9ffaa
hassaanhub454@gmail.com <hassaanali994@gmail.com>
2025-08-25 08:30:06 +0500

    uncomplete

---
+++ javascript/class_41/app.js
@@ -0,0 +1,36 @@
+// CallBack Hell
+
+// Start Shopping...
+// 1: Order placed
+// 2: Payment done
+// 3: Shipping started
+// 4: Delivery checked
+// 5: Wallet updated
+// 6: Final status: SUCCESS
+
+// console.log("Start shopping");
+
+// setTimeout(() => {
+//     console.log("Step 1: Order placed");
+//     setTimeout(() => {
+//         console.log("Step 2: Payment done");
+//         setTimeout(() => {
+//             console.log("Step 3: Shipping started");
+//             setTimeout(() => {
+//                 console.log("Step 4: Delivery checked");
+//                 setTimeout(() => {
+//                     console.log("Step 5: Wallet updated");
+//                     setTimeout(() => {
+//                         console.log("Step 6: Final status: SUCCESS");
+//                     }, 1000)
+//                 }, 1000)
+//             }, 1000);
+//         }, 1000)
+//     }, 1000)
+// }, 1000)
+
+
+
+
+
+// PROMISES
