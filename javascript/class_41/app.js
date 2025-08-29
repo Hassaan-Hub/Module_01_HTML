0d04dd07f5380d8f61286e4c5a100681af93b201
hassaanhub454@gmail.com <hassaanali994@gmail.com>
2025-08-29 18:37:06 +0500

    updated

--- javascript/class_41/app.js
+++ javascript/class_41/app.js
@@ -34,3 +34,50 @@
 
 
 // PROMISES
+
+// const api = {
+//     Order: c => Promise.resolve("ORDER123"),
+//     Payment: o => Promise.resolve("PAYMENT456"),
+//     Shipping: p => Promise.resolve("TRACK789"),
+//     CheckDelivery: t => Promise.resolve("Delivered")
+// }
+
+// api.Order(["Shoes"])
+//     .then(api.Payment)
+//     .then(api.Shipping)
+//     .then(api.CheckDelivery)
+//     .then(status => console.log("Delivery Status: ", status))
+//     .catch(console.error)
+
+
+
+
+
+
+// Async / Await (Most Readable)
+
+// const api = {
+//     Order: () => Promise.resolve("ORDER123"),
+//     Payment: () => Promise.resolve("PAYMENT456"),
+//     Shipping: () => Promise.resolve("TRACK879"),
+//     CheckDelivery: () => Promise.resolve("Delivered")
+// };
+
+// (async () => console.log("Delivery Status: ", await api.CheckDelivery(await api.Shipping(await api.Payment(await api.Order())))))();
+
+
+
+
+const user = fatch('https://api.github.com/users/Hassaan-Hub')       //  not actual value
+
+user.then(function (data) {
+    console.log(data);
+})
+
+
+fatch('https://api.github.com/users/Hassaan-Hub')                        // actual value
+    .then(Response => Response.json())  // convert Response to JSON
+    .then(data => console.log(data))     // actual data
+    .catch(error => console.error(error));
+
+
