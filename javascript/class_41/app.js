ddcf3a19d95ed3cc8352c9a413beb173b1f7439e
hassaanhub454@gmail.com <hassaanali994@gmail.com>
2025-08-31 22:32:27 +0500

    refactor: simplify API calls and enhance readability

--- javascript/class_41/app.js
+++ javascript/class_41/app.js
@@ -1,12 +1,5 @@
 // CallBack Hell
 
-// Start Shopping...
-// 1: Order placed
-// 2: Payment done
-// 3: Shipping started
-// 4: Delivery checked
-// 5: Wallet updated
-// 6: Final status: SUCCESS
 
 // console.log("Start shopping");
 
@@ -33,14 +26,73 @@
 
 
 
+
+// Simulated API
+
+
+const api = {
+    Order: c => Promise.resolve("ORDER123"),
+    Payment: o => Promise.resolve("PAYMENT456"),
+    Shipping: p => Promise.resolve("TRACK789"),
+    CheckDelivery: t => Promise.resolve("Delivered"),
+    updateWallet: s => Promise.resolve("Wallet Updated"),
+    walletSatus: w => Promise.resolve("SUCCESS")
+}
+
+let cart = ["Shoes", "Shirt", "Pant"];
+
+let order = api.Order(cart, function (orderId) {
+    api.Payment(orderId, function (paymentInfo) {
+        api.Shipping(paymentInfo, function (trackingId) {
+            api.CheckDelivery(trackingId, function (status) {
+                api.updateWallet(status, function (finalStatus) {
+                    api.walletSatus(finalStatus, function (status) {
+                        console.log("Final Status: ", status);
+                    })
+                })
+            })
+        })
+    })
+})
+
+api
+    .Order(["Shoes"])
+    .then((orderId) => {
+        console.log("Order ID: ", orderId);
+        return api.Payment(orderId);
+    })
+    .then((paymentInfo) => {
+        console.log("Payment Info: ", paymentInfo);
+        return api.Shipping(paymentInfo);
+    })
+    .then((trackingId) => {
+        console.log("Tracking ID: ", trackingId);
+        return api.CheckDelivery(trackingId);
+    })
+    .then((status) => {
+        console.log("Delivery Status: ", status);
+        return api.updateWallet(status);
+    })
+    .then((walletSatus) => {
+        console.log("Wallet Status: ", walletSatus);
+        return api.walletSatus(walletSatus);
+    })
+    .then((finalStatus) => {
+        console.log("Final Status: ", finalStatus);
+    })
+
+
+
+
 // PROMISES
 
-// const api = {
-//     Order: c => Promise.resolve("ORDER123"),
-//     Payment: o => Promise.resolve("PAYMENT456"),
-//     Shipping: p => Promise.resolve("TRACK789"),
-//     CheckDelivery: t => Promise.resolve("Delivered")
-// }
+// Start Shopping...
+// 1: Order placed
+// 2: Payment done
+// 3: Shipping started
+// 4: Delivery checked
+// 5: Wallet updated
+// 6: Final status: SUCCESS
 
 // api.Order(["Shoes"])
 //     .then(api.Payment)
@@ -68,16 +120,16 @@
 
 
 
-const user = fatch('https://api.github.com/users/Hassaan-Hub')       //  not actual value
+// const user = fatch('https://api.github.com/users/Hassaan-Hub')       //  not actual value
 
-user.then(function (data) {
-    console.log(data);
-})
+// user.then(function (data) {
+//     console.log(data);
+// })
 
 
-fatch('https://api.github.com/users/Hassaan-Hub')                        // actual value
-    .then(Response => Response.json())  // convert Response to JSON
-    .then(data => console.log(data))     // actual data
-    .catch(error => console.error(error));
+// fatch('https://api.github.com/users/Hassaan-Hub')                        // actual value
+//     .then(Response => Response.json())  // convert Response to JSON
+//     .then(data => console.log(data))     // actual data
+//     .catch(error => console.error(error));
 
 
