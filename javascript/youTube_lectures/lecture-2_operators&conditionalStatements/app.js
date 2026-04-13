f676c71c789acda63f1dec65cec99168030a7d0d
hassaanhub454@gmail.com <hassaanali994@gmail.com>
2026-04-13 21:38:20 +0500

    feat: add initial HTML and JavaScript files for lectures on variables, operators, and loops

---
+++ javascript/youTube_lectures/lecture-2_operators&conditionalStatements/app.js
@@ -0,0 +1,75 @@
+// comments is js
+// part of code which is not executed
+
+// this is a single line comment
+
+/* this is a multi-line
+    comment. */
+
+
+
+
+// operators in js
+// arithmetic operators
+
+// let a = 5;
+// let b = 2;
+
+// console.log("a + b = ", a + b);
+// console.log("a - b = ", a - b);
+// console.log("a * b = ", a * b);
+// console.log("a / b = ", a / b);
+// console.log("a % b = ", a % b);
+// console.log("a ** b = ", a ** b);
+
+
+
+
+// operators in js
+// logical operators
+
+
+// let a = 6;
+// let b = 5;
+
+// console.log("!(a < b) = ", !(a < b));
+// console.log("!(a < b) = ", !(a < b));
+
+
+
+
+// conditional statements
+
+
+// let mode = "silver";
+// let color;
+
+// if (mode === "sky"){
+//     color = "blue"
+// }else if(mode === "dark"){
+//     color = "black"
+// }else if(mode === "red"){
+//     color = "laal"
+// }else{
+//     color = "not have this color"
+// }
+
+// console.log(color);
+
+
+// let score = 60;
+// let grade;
+
+// if (score >= 90 && score <= 100){
+//     grade = "A"
+// }else if(score >= 70 && score <= 89){
+//     grade = "B"
+// }else if(score >= 60 && score <= 69){
+//     grade = "C"
+// }else if(score >= 50 && score <= 69){
+//     grade = "D"
+// }else if(score >= 0 && score <= 49){
+//     grade = "F"
+// }
+
+// console.log(grade);
---
+++ javascript/youTube_lectures/lecture-3_Loops&Strings/app.js
@@ -0,0 +1,45 @@
+// for-of loop
+
+
+// let str = "JavaScript"
+
+// let size = 0;
+// for (var i of str){
+//     console.log(i);
+//     size++
+// }
+
+// console.log("string size = ", size);
+
+
+
+
+// Practice Qs-1
+
+// for (let i = 0; i <=100; i++){
+//     if(i % 2 === 0){
+//         console.log("even number ", i);
+//     }else{
+//         console.log("odd number ", i);
+//     }
+// }
+
+
+
+
+// Practice Qs-2
+
+// let gameNum = 7;
+// let userNum = prompt("Guess the game number..")
+
+// while(userNum != gameNum){
+//     userNum = prompt("You enterred wrong number. Guess again.")    
+// }
+// console.log("congratulations, you enterred the right number.");
+
+
+
+// let fullName = prompt("enter your full name");
+
+// let result = "@" + fullName + fullName.length;
+// console.log(result);
