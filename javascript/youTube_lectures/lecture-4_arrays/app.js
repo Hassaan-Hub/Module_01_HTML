4be9a1787d3746ef8ece1c4cebd7705630549708
hassaanhub454@gmail.com <hassaanali994@gmail.com>
2026-04-14 21:56:16 +0500

    updated

---
+++ javascript/youTube_lectures/lecture-4_arrays/app.js
@@ -0,0 +1,29 @@
+// Arrays
+
+
+// let heroes = ["ironman", "hulk", "thor", "batman"]
+
+// for(var i = 0; i <heroes.length; i++){
+//     console.log(heroes[i]);
+// }
+
+// for (var i in heroes){      //  output in length
+//     console.log(i);
+// }
+
+// for (var i of heroes){      //  output of pure indexs
+//     console.log(i);
+// }
+
+
+
+// var marks = [23,42,65,76,87,98,19]
+// var sum = 0;
+
+// for(var i of marks){
+//     sum += i
+// }
+
+// let avg = sum / marks.length;
+
+// console.log(`average marks of the class = ${avg.toFixed(2)}`);
