d0d44dfe4f8e65ac5bc08edfdb7738159c859022
hassaanhub454@gmail.com <hassaanali994@gmail.com>
2026-04-19 21:32:17 +0500

    feat: expand examples for array methods and functions in app.js

--- javascript/youTube_lectures/lecture-5_functions&methods/app.js
+++ javascript/youTube_lectures/lecture-5_functions&methods/app.js
@@ -22,8 +22,122 @@
 // Arrow Function
 // compact way of writing a function
 
-let newArrow = (a,b) => {
-    console.log(a-b);
-}
-newArrow(5,2)
+// let newArrow = (a,b) => {
+//     console.log(a-b);
+// }
+// newArrow(5,2)
 
+// const arrowSum = (a, b) => {
+//     console.log(a + b);
+// }
+// arrowSum(5, 10);
+
+// // multiplication function
+// const arrowMul = (a, b) => {
+//     console.log(a * b);
+// }
+// arrowMul(3, 2);
+
+
+// const printName = a => console.log(a);
+// printName("hassaan");
+
+
+// lets Practice
+// Qs1. create a function using the "function" keyword that takes a string as an argument &
+// returns the number of vowels in that string. 
+
+// function findvowels(a) {
+//     let count = 0;
+//     for (let char of a) {
+//         if (char == "a" || char == "e" || char == "i" || char == "o" || char == "u") {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+// let res = findvowels("hassaan")
+// console.log(res);
+
+
+// Qs2. create an arrow function to perform the same task.
+
+// const findVowels = (a) => {
+//     let count = 0;
+
+//     for (let char of a) {
+//         if (char == "a" || char == "e" || char == "i" || char == "o" || char == "u") {
+//             count++;
+//         }
+//     }
+//     return count;
+// }
+
+// let res = findVowels("hello saylani")
+// console.log(res);
+
+
+// forEach loop in Arrays   arr.forEach(callBackFunction)
+
+
+// let arr = [1, 2, 3, 4, 5];
+
+// arr.forEach((val) =>{
+//     console.log(val);
+// })
+
+
+// let arr = ["karachi", "islamabad", "punjab"];
+
+// arr.forEach((val, idx, arr) => {
+//     console.log(val.toUpperCase(), idx, arr);
+// })
+
+
+
+// Qs3. for a given array of numbers, print the square of each number using forEach loop.
+
+// let arr = [2, 4, 5, 6, 7, 8, 9, 1, 4, 13]
+
+// arr.forEach(function square(num) {
+//     console.log(num * num);
+// })
+
+
+// let arr = [2, 4, 5, 6, 7, 8, 9, 1, 4, 13]
+
+// arr.forEach((num) => {
+//     console.log(num - 2);
+// })
+
+
+
+// some more array methods
+// map()
+
+// let arr = ["karachi", "islamabad", "punjab"];
+
+// let result = arr.map((val) => {
+//     return val;
+// })
+// console.log(result);
+
+
+// filter()
+
+// let arr1 = [1, 2, 3, 4, 5, 6, 7];
+
+// let newArr = arr1.filter((val) => {
+//     return val % 2 == 0
+// })
+// console.log(newArr);
+
+
+// reduce()
+
+// let arr2 = [1, 2, 3, 4, 5, 6, 7];
+
+// let result1 = arr2.reduce((pre, cur) => {
+//     return pre > cur ? pre : cur;
+// })
+// console.log(result1);
