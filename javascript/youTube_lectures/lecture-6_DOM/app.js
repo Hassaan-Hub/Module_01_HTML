705ed038f9ab9ff38f3769aa871763789b46f0a8
hassaanhub454@gmail.com <hassaanali994@gmail.com>
2026-04-20 22:12:12 +0500

    updated

---
+++ javascript/youTube_lectures/lecture-6_DOM/app.js
@@ -0,0 +1,43 @@
+// console.dir(document.body.childNodes[3]);
+
+
+// document.body.childNodes[3].innerText = "abcd";
+
+
+// selecting with id
+
+// let id = document.getElementById("myId");
+// console.dir(id);
+
+
+// selecting with class
+
+// let clas = document.getElementsByClassName("myClass")
+// console.log(clas);
+
+
+// selecting with tag
+
+// let tag = document.getElementsByTagName("p")
+// console.log(tag);
+
+
+// selecting with querySelector
+
+// let query = document.querySelector(".myClass")
+// console.log(query);
+
+
+// selecting with querySelectorAll
+
+// let queryAll = document.querySelectorAll(".myClass")
+// console.log(queryAll);
+
+
+
+// let elements = document.querySelector("h1")
+// console.log(elements);
+
+
+
+console.dir(document.body.firstChild);
