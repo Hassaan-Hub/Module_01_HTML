e015d9b97e2fb1607eb6a733eb439b77afbed996
hassaanhub454@gmail.com <hassaanali994@gmail.com>
2026-04-21 17:53:28 +0500

    feat: add additional examples and comments for DOM manipulation in app.js and index.html; update style.css for clarity

--- javascript/youTube_lectures/lecture-6_DOM/app.js
+++ javascript/youTube_lectures/lecture-6_DOM/app.js
@@ -40,4 +40,42 @@
 
 
 
-console.dir(document.body.firstChild);
+// console.dir(document.body.firstChild);
+// console.dir(document.body.childNodes[5]);
+
+
+
+
+// let div = document.querySelector("div")
+// console.dir(div);
+
+// div.innerText       // output 'fruits\nmango\norange\nlitchi'
+// div.innerHTML       // '\n     <h2>fruits</h2>\n    <ul>\n  <li>mango</li>\n  <li>orange</li>\n    <li>litchi</li>\n  </ul>\n'
+
+
+// let heading = document.querySelector("h1")
+
+// heading.innerText       // output ''
+// heading.textContent     // output 'old heading!'
+
+
+// Qs. create a H2 heading element with text - "Hello JavaScript". Append "from Apna College
+// students" to this text using JS.
+
+
+// let heading = document.querySelector("h2")
+// heading.innerText += " from Apna College students"
+
+
+
+// Qs. create 3 divs with common class name - "box". Access them & add some unique text to
+// each of them.
+
+
+// let box = document.querySelectorAll(".boxs")
+// let idx = 1;
+
+// for(let i of box){
+//     i.innerText = `new unique value ${idx}`
+//     idx++
+// }
