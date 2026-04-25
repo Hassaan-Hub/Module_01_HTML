9da7b43bb5c034fa4072841b3bdcf281054d5178
hassaanhub454@gmail.com <hassaanali994@gmail.com>
2026-04-25 12:27:10 +0500

    feat: migrate lecture 6 DOM files to new directory structure; remove old files and add new examples

---
+++ javascript/youTube_lectures/lecture-7-DOM-p2/app.js
@@ -0,0 +1,79 @@
+// let div = document.querySelector("div")
+// console.log(div);
+
+// let id = div.getAttribute("id")
+// console.log(id);
+
+
+// let id1 = div.getAttribute("name");
+// console.log(id1);
+
+
+
+// let para = document.querySelector("p");
+// console.log(para.setAttribute("class", "newClass"));
+
+
+// let div = document.querySelector("div")
+// div.setAttribute("id","hello");
+// console.log(div);
+// div.style.backgroundColor = "purple"
+// div.style.fontSize = "26px"
+// div.innerText = "hello!"
+
+
+
+// let div = document.querySelector("#box")
+// let button = document.createElement("button");
+// button.innerText = "click me"
+
+// div.append(button)
+// div.prepend(button)
+// div.before(button)
+// div.after(button)
+// div.remove()
+
+
+
+// let newHeading = document.createElement("h1");
+// newHeading.innerHTML = `<i>Hi, I am new!</i>`;
+// let body = document.querySelector("body")
+
+
+
+// let para = document.querySelector("p");
+// para.remove()
+
+
+
+// let div = document.querySelector("#box")
+// let para = document.querySelector("p");
+// div.removeChild(para)
+
+
+
+// Qs. create a new button element. give it a text "click me", background color of red
+// & text color of white.
+// insert the button as the first element inside the body tag. 
+
+// let btn = document.createElement("button");
+// btn.innerText = "click me";
+// btn.style.backgroundColor = "red";
+// btn.style.color = "white";
+
+// let body = document.querySelector("body")
+// body.prepend(btn)
+
+
+
+// Qs. create a <p> tag in html,give it a class & some styling.
+// Now create a new class in css and try to append this class to the <p> element.
+// did you notice, how you overwrite the class name when you add a new one?
+// solve this problem using classList.
+
+
+// let para = document.querySelector("p");
+// para.setAttribute("class", "newClass");
+
+// para.classList.remove("para")
+// para.classList.add("newClass")
