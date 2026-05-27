065e8cdd51e670fddb5c524b49f13961768754c4
hassaanhub454@gmail.com <hassaanali994@gmail.com>
2026-05-27 14:58:07 +0500

    Add initial setup for React project with Vite
    - Created main entry point (main.jsx) for the React application.
    - Added global styles in index.css for consistent styling.
    - Included React and Vite configurations in vite.config.js.
    - Added SVG assets for React and Vite logos.

---
+++ react-full-course/02-components/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="card">
+        <h1>Card Title</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsum.</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
