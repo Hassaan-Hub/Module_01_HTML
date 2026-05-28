40cbb8f46b8f078cc9159fe45a705d9fa80e2060
hassaanhub454@gmail.com <hassaanali994@gmail.com>
2026-05-28 13:47:10 +0500

    feat: refactor App component to utilize Card component and enhance layout

--- react-full-course/02-components/src/App.jsx
+++ react-full-course/02-components/src/App.jsx
@@ -1,12 +1,113 @@
-import React from 'react'
+// import React from 'react'
+// import second from './components/card.jsx'
 
-const App = () => {
+// const App = () => {
+//   return (
+//     <div>
+//       <div className="card">
+//         <h1>Card Title</h1>
+//         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsum.</p>
+//       </div>
+//       {second()}
+
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+// import React from 'react'
+
+// function App() {
+//   const user = "Daniyal"
+//   const age = 22;
+
+//   return (
+//     <div>
+//       <h1>Hello, Guys I am {user}</h1>
+//       <h1>and I am {age} years </h1>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+
+
+import React from 'react'
+import Second from './components/card.jsx'
+
+function App() {
   return (
-    <div>
-      <div className="card">
-        <h1>Card Title</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsum.</p>
-      </div>
+    <div className='mainCard'>
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
+      <Second />
     </div>
   )
 }
---
+++ react-full-course/02-components/src/components/card.jsx
@@ -0,0 +1,8 @@
+function Card(){
+    return <div className="card">
+        <h1>Card Title</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ipsum.</p>
+      </div>
+}
+
+export default Card;
