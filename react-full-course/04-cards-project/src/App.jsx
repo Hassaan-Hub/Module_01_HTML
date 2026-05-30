13f78baf62e75c448aa4e7c8be76aaa9c130b9f6
hassaanhub454@gmail.com <hassaanali994@gmail.com>
2026-05-30 19:15:59 +0500

    feat: update card component layout and styling for improved UI

--- react-full-course/04-cards-project/src/App.jsx
+++ react-full-course/04-cards-project/src/App.jsx
@@ -7,11 +7,22 @@ const App = () => {
       <div className='card'>
         <div className="top">
           <img src="https://static.vecteezy.com/system/resources/thumbnails/019/136/322/small_2x/amazon-logo-amazon-icon-free-free-vector.jpg" alt="" />
-          <button>Save <RiBookmarkLine /></button>
+          <button>Save <RiBookmarkLine size={10} /></button>
         </div>
         <div className="center">
           <h3>Amazon <span>5 days ago</span></h3>
           <h2>Senior UI/UX Designer</h2>
+          <div className='tag'>
+            <h4>Part Time</h4>
+            <h4>Senior Level</h4>
+          </div>
+        </div>
+        <div className="bottom">
+          <div>
+            <h3>$120/hr</h3>
+            <p>Mumbai, India</p>
+          </div>
+          <button>Apply Now</button>
         </div>
       </div>
     </div>
