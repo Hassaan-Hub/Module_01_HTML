64db9c9559b50a75adb2d3892e991a385de31b89
hassaanhub454@gmail.com <hassaanali994@gmail.com>
2026-06-08 22:59:15 +0500

    feat: add additional user satisfaction images to the App component and update RightContent styling

--- react-full-course/07-ui-project/src/App.jsx
+++ react-full-course/07-ui-project/src/App.jsx
@@ -18,6 +18,31 @@ const App = () => {
       img: 'https://images.unsplash.com/photo-1546979859-b7d45d499dd6?q=80&w=685&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
       intro: '',
       tag: 'Unsatisfied'
+    },
+    {
+      img: 'https://plus.unsplash.com/premium_photo-1779431739556-db15915dbc5a?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      intro: '',
+      tag: 'very satisfied'
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1664789111672-9cf3ada6c20d?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      intro: '',
+      tag: 'satisfied'
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1654272177889-1dd3b0d6aca2?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      intro: '',
+      tag: 'amazingly satisfied'
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1490677739661-d3395897c0a7?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      intro: '',
+      tag: 'amazingly satisfied'
+    },
+    {
+      img: 'https://images.unsplash.com/photo-1759272548449-7b689a81c8fb?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      intro: '',
+      tag: 'seprately satisfied'
     }
   ]
   return (
--- react-full-course/07-ui-project/src/components/section1/RightContent.jsx
+++ react-full-course/07-ui-project/src/components/section1/RightContent.jsx
@@ -5,7 +5,7 @@ const RightContent = (props) => {
   console.log(props);
   
   return (
-    <div className='h-[80vh] w-2/3 p-5 flex flex-nowrap gap-10 overflow-x-auto rounded-4xl'>
+    <div className='h-[80vh] w-2/3 p-5 flex flex-nowrap gap-10 scrollbar-none overflow-x-auto rounded-4xl'>
         {props.users.map(function(elem){
           return <RightCard image={elem.img} />
         })}
