a241f9b1608b08731e26cde8822b6577eeff42f3
hassaanhub454@gmail.com <hassaanali994@gmail.com>
2026-06-11 12:38:58 +0500

    updated

--- react-full-course/09-useState/src/App.jsx
+++ react-full-course/09-useState/src/App.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+
+  // let a = 20;
+
+  // const changeA = () => {
+  //   console.log(a);
+  //   a++
+  //   console.log(a);
+  // }
+  // const [name, setName] = useState('hassaan')
+
+
+
+
+  // const [num, setNum] = useState(10)
+  // const [username, setUsername] = useState('hassaan')
+  // const [users, setUsers] = useState([10, 20, 30])
+
+  // const changeNum = () => {
+  //   setNum(30)
+  //   setUsername('kaif payi')
+  //   setUsers([40, 50, 60])
+  // }
+
+
+
+  const [num, setNum] = useState(0)
+
+  const increaseNum = ()=>{
+    setNum(num + 1)
+  }
+  const decreaseNum = ()=>{
+    setNum(num - 1)
+  }
+  const increaseNumFive = ()=>{
+    setNum(num + 5)
+  }
+
   return (
-    <div>App1</div>
+    <div>
+      <h1>{num}</h1>
+      <button onClick={increaseNum}>increase</button>
+      <button onClick={decreaseNum}>decrease</button><br />
+      <button onClick={increaseNumFive}>increase five</button>
+
+
+      {/* <h1>Value of a is {num} <br /> {users} <br /> Value of user is {username} </h1>
+      <button onClick={changeNum}>Click</button> */}
+    </div>
   )
 }
 
