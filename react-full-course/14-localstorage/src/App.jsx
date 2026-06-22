8346a53c477f9777ff9b04f20643f387b94401a8
hassaanhub454@gmail.com <hassaanali994@gmail.com>
2026-06-22 13:38:15 +0500

    Add initial setup for React project with Vite
    - Created main entry point (main.jsx) for the React application.
    - Added global styles in index.css for consistent layout.
    - Included React and Vite SVG assets for branding.
    - Configured Vite with a basic setup in vite.config.js.

---
+++ react-full-course/14-localstorage/src/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const App = () => {
+
+  // localStorage.setItem('user', 'hassaan')
+  // localStorage.setItem('age', 20)
+  
+  // const user = localStorage.getItem('user')
+  // console.log(user);
+  // localStorage.removeItem('age')
+  // localStorage.removeItem('user')
+
+
+  // const user = {
+  //   username: 'hassaan',
+  //   age: 12,
+  //   city: 'rajisthan'
+  // }
+
+  // localStorage.setItem('user', JSON.stringify(user))
+
+  // const user = JSON.parse(localStorage.getItem('user'))
+  // console.log(user);
+  
+  
+
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
