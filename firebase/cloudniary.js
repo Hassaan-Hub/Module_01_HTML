391f91cc248bfa986c3d791a24da7b47fd63b1a0
hassaanhub454@gmail.com <hassaanali994@gmail.com>
2026-07-26 18:10:08 +0500

    updated

--- firebase/cloudniary.js
+++ firebase/cloudniary.js
@@ -12,15 +12,5 @@ export async function uploadImage(formData) {
 
     const { secure_url } = await response.json()
     return secure_url;
-
-    // .then((response) => {
-    //     return response.text();
-    // })
-    // .then((data) => {
-    //     document.getElementById('data').innerHTML += data;
-    // })
-    // .catch((error)=>{
-    //     console.log(error,"==>> error");
-    // })
 }
 
