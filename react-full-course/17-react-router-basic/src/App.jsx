aacc195c8ed8c6829f52fe784bf3f5554c668d99
hassaanhub454@gmail.com <hassaanali994@gmail.com>
2026-08-17 17:10:33 +0500

    feat: add initial setup for React Router with basic pages and navigation
    - Created Navbar component for navigation links
    - Added Home, About, Contact, and Product pages
    - Implemented AppRouter for routing between pages
    - Included SVG assets for React and Vite logos
    - Configured Vite with Tailwind CSS
    - Added global CSS styles

---
+++ react-full-course/17-react-router-basic/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AppRouter from './routes/AppRouter'
+import Navbar from './component/Navbar'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <AppRouter />
+    </div>
+  )
+}
+
+export default App
